7712481c765ad42c519ca233f5558bd7e87cbd61
Matvey Sudomoykin <me@matveymatvey.com>
2025-03-14 20:35:55 +0300

    nice

--- DitherJS/palettes.js
+++ DitherJS/palettes.js
@@ -10,11 +10,19 @@ export const PALETTES = {
     [139, 172, 15], // Yellow-green
     [155, 188, 15], // Light yellow-green
   ],
-  sepia: [
+  ssepia: [
     [0, 0, 0], // Black
     [112, 66, 20], // Dark sepia
     [168, 100, 30], // Medium sepia
     [224, 134, 40], // Light sepia
     [255, 255, 255], // White
+  ],
+
+  sepia: [
+    [120, 0, 0], // Black
+    [221, 66, 20], // Dark sepia
+    [122, 21, 30], // Medium sepia
+    [214, 134, 40], // Light sepia
+    [255, 255, 120], // White
   ],
 };
--- example/index.js
+++ example/index.js
@@ -5,15 +5,23 @@ const params = document.createElement("div");
 params.classList.add("params-note");
 
 const img = new Image();
-img.src = "example2.png";
+img.src = "./images/CAR.jpg";
 img.onload = () => {
   const options = {
-    // algorithm: "floydSteinberg",
     algorithm: "bayer",
-    palette: "grayscale",
-    scale: .8 // Reduce the intensity of the dithering effect
+    palette: "sepia",
+    algorithmOptions: {
+      thresholdScale: 16.727, //bayer
+      factor: 2, //atkinson
+      
+      //floyd
+      rightFactor: .1,
+      bottomLeftFactor: 11.911,
+      bottomFactor: .1,
+      bottomRightFactor: 21.1,
+    },
+    scale: .058912424131313222599999,
   };
-
   const ditheredCanvas = dither.applyDithering(img, options);
   params.textContent = JSON.stringify(options);
   document.body.appendChild(params);
